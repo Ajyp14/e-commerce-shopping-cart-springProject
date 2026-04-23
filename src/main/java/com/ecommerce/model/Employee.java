e5921f9f63709dadaaaa002275ee348cecf52185
Ajayp <ajyp1404@gmail.com>
2026-04-23 21:56:21 +0530

    create employe entity

---
+++ src/main/java/com/ecommerce/model/Employee.java
@@ -0,0 +1,31 @@
+package com.ecommerce.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="first_name",nullable = false)
+	private String first_name;
+	
+	@Column(name="last_name",nullable = false)
+	private String last_name;
+	
+	@Column(name="email")
+	private String email;
+	
+	
+
+}
