d3e3a7c56a8adcaef6668135c43de428aea4fce1
Ajayp <ajyp1404@gmail.com>
2026-04-24 17:24:35 +0530

    create employee repository and exception class

---
+++ src/main/java/com/ecommerce/exception/ResourceNotFoundException.java
@@ -0,0 +1,48 @@
+package com.ecommerce.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	private String resourcename;
+	private String fieldname;
+	private String fieldValues;
+	
+	public ResourceNotFoundException(String resourcename, String fieldname, String fieldValues) {
+		super(String.format("%s not found with %s : '%s'",resourcename,fieldname,fieldValues));
+		this.resourcename = resourcename;
+		this.fieldname = fieldname;
+		this.fieldValues = fieldValues;
+	}
+
+	public String getResourcename() {
+		return resourcename;
+	}
+
+	public void setResourcename(String resourcename) {
+		this.resourcename = resourcename;
+	}
+
+	public String getFieldname() {
+		return fieldname;
+	}
+
+	public void setFieldname(String fieldname) {
+		this.fieldname = fieldname;
+	}
+
+	public String getFieldValues() {
+		return fieldValues;
+	}
+
+	public void setFieldValues(String fieldValues) {
+		this.fieldValues = fieldValues;
+	}
+	
+	
+	
+	
+}
